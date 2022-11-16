e14cc3f2470fc58241a6f4066dcb9523bc555d9f
schulzy <schulzyy@gmail.com>
2022-11-16 22:48:20 +0100

    prepare day12

---
+++ AdventOfCode2021/Schulteisz.AdventOfCode2021.Day12/PassagePathing.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Schulteisz.AdventOfCode2021.Interfaces;
+
+namespace Schulteisz.AdventOfCode2021.Day12
+{
+	public class PassagePathing : IDailyTask
+	{
+        private IContentParser _contentParser;
+
+        public PassagePathing()
+		{
+		}
+
+        public string Name => "Passage Pathing";
+
+        public long Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ AdventOfCode2021/Schulteisz.AdventOfCode2021.Day12/PassagePathingHard.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Schulteisz.AdventOfCode2021.Interfaces;
+
+namespace Schulteisz.AdventOfCode2021.Day12
+{
+	public class PassagePathingHard : IDailyTask
+    {
+        private IContentParser _contentParser;
+
+        public PassagePathingHard()
+		{
+		}
+
+        public string Name => "Passage Pathing Hard";
+
+        public long Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
